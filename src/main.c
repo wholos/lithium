a918c4edf1d8f7d4beb63463d65e1dedf8711561
holos <holosnikitatt@gmail.com>
2026-01-05 19:10:13 +0200

    Create main.c

---
+++ src/main.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include"colorlib.h"
+const char *repository="https://raw.githubusercontent.com/wholos/HronyPackages/main/binary/";
+int instl(int argc,char *argv[]){
+    if(argc < 3){
+        printf("use lithium load <pkg>\n");
+        return 0;
+    }
+    for(int i=2; i<argc;i++){
+        char ttyi[256];
+        char chmd[256];
+        printf(GREEN"lithium: installing %s\n"reset, argv[i]);
+        snprintf(ttyi,sizeof(ttyi),"wget -q --directory=/bin/ %s%s",repository,argv[i]);
+        snprintf(chmd,sizeof(chmd),"chmod +x /bin/%s",argv[i]);
+        system(ttyi);
+        system(chmd);
+        return 0;
+    }
+}
+int rmv(int argc,char *argv[]){
+    if(argc < 3){
+        printf("use lithium kill <pkg>\n");
+        return 0;
+    }
+    for(int i=2; i<argc;i++){
+        char ttyr[256];
+        snprintf(ttyr,sizeof(ttyr),"rm -rf /bin/%s",argv[i]);
+        system(ttyr);
+        return 0;
+    }
+}
+int main(int argc,char *argv[]){
+    if(argc < 2) {
+        printf("lithium: use --help for documentation if use\n");
+        return 0;
+    }
+    if(strcmp(argv[1],"--help")==0){
+        printf("lithium.\n");
+        printf("load: install package to /bin/ directory\n");
+        printf("kill: remove package from /bin/ directory\n");
+        return 0;
+    }
+    if(strcmp(argv[1],"load")==0){
+        instl(argc,argv);
+        return 0;
+    }
+    if(strcmp(argv[1],"kill")==0){
+        rmv(argc,argv);
+        return 0;
+    }
+    printf("lithium: argument %s not found\n",argv[1]);
+}
